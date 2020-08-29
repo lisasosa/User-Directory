3b0ea02442b9ac0b87ef918e5184faa8df95ebe6
Elizabeth Sosa <sosa.elizabeth@ymail.com>
2020-08-28 23:10:06 -0500

    Updates

---
+++ precache-manifest.2d1da5369826f8e527317c07b741111a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0afecee4ed04689f5c28e685af855092",
+    "url": "/User-Directory/index.html"
+  },
+  {
+    "revision": "f30b2843ff67117eed6f",
+    "url": "/User-Directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "12797ddfb243fb4075b6",
+    "url": "/User-Directory/static/js/2.acfc0cfe.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/User-Directory/static/js/2.acfc0cfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f30b2843ff67117eed6f",
+    "url": "/User-Directory/static/js/main.a524586d.chunk.js"
+  },
+  {
+    "revision": "f8d550d65fe1b61ea334",
+    "url": "/User-Directory/static/js/runtime-main.b3b293a7.js"
+  }
+]);
