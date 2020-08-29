fc1e569e8075df8a82a758ed5cc20e13f421031f
Elizabeth Sosa <sosa.elizabeth@ymail.com>
2020-08-28 21:28:51 -0500

    added search option

--- user-directory/src/App.js
+++ user-directory/src/App.js
@@ -8,23 +8,41 @@ import * as moment from 'moment'
    constructor(props) {
      super(props) 
      this.state = {
+       searchValue: "",
        results: []
      }
    }
    componentDidMount(){
+     //life cycle method
     fetch('https://randomuser.me/api/?results=50')
     
     .then(response => response.json())
     .then(data => {
       console.log(data)
-      this.setState({results:data.results})})
+      this.setState({results:data.results})
+    })
 
+   }
+   search =  (e) => {
+     console.log(e.target.value)
+     this.setState({searchValue: e.target.value})
    }
  
   render(){
+    var results = this.state.results
+    var searchValue = this.state.searchValue.toLowerCase()
+    if (searchValue) {
+      results = results.filter((item) => {
+        const fullName = (item.name.first + " " + item.name.last).toLowerCase()
+    
+        return fullName.includes(searchValue)
+        //returns true or false
+      }) 
+    }
     return (
       <div className="App">
-      <People results = {this.state.results}/>
+      <input onKeyUp = {this.search}/>
+      <People results = {results}/>
       </div>
     );
   }
